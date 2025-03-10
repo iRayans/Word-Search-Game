97f686b4641d2628ee5bdef7e11b7e89188afaec
rharbi <iirharbi@gmail.com>
2025-03-10 14:18:01 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        final int GRID_SIZE = 10;
+        char[][] grid = new char[GRID_SIZE][GRID_SIZE];
+        List<String> words = Arrays.asList("ONE", "TWO", "THREE", "FOUR");
+    }
+}
